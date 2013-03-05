92b0a258494ee14aaa3738cad38fc68badd83c7f
Grundlefleck <grundlefleck@gmail.com>
2013-03-05 21:56:05 +0000

    Add some more scala examples. Several of the tests don't pass.

--- src/main/scala/org/mutabilitydetector/scalaclient/SomeCaseClass.scala
+++
@@ -1,3 +0,0 @@
-package org.mutabilitydetector.scalaclient
-
-class SomeCaseClass(name: String, age: Int)
--- src/test/scala/org/mutabilitydetector/scalaclient/ImmutabilityTests.scala
+++ src/test/scala/org/mutabilitydetector/scalaclient/ImmutabilityTests.scala
@@ -1,13 +1,36 @@
 package org.mutabilitydetector.scalaclient
 
-import org.scalatest._
-import matchers.ShouldMatchers
+import org.junit.runner.RunWith
 import org.mutabilitydetector.unittesting.MutabilityAssert
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.matchers.ShouldMatchers
+import org.mutabilitydetector.unittesting.MutabilityAssert._
 
-@org.junit.runner.RunWith(classOf[org.scalatest.junit.JUnitRunner])
+@RunWith(classOf[JUnitRunner])
 class ImmutabilityTests extends FunSuite with ShouldMatchers {
 
-  test("be immutable") {
-    MutabilityAssert.assertImmutable(classOf[SomeCaseClass])
+  test("non final case class") {
+    assertImmutable(classOf[CaseClass])
+  }
+
+  test("final case class") {
+    assertImmutable(classOf[FinalCaseClass])
+  }
+
+  test("final immutable class") {
+    assertImmutable(classOf[ImmutableClass])
+  }
+  
+  test("final mutable class with reassignable fields") {
+    assertImmutable(classOf[ReassignableFields])
+  }
+  
+  test("final immutable class with immutable list field") {
+    assertImmutable(classOf[ImmutableWithListField])
+  }
+  
+  test("scala.collection.immutable.List - abstract") {
+    assertImmutable(classOf[scala.collection.immutable.List[_]])
   }
 }
