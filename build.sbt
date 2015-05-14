94281ac5df0ffa2bd8f8796484d29635bc49febf
Graham Allan <graham.allan@timgroup.com>
2015-05-14 23:01:00 +0100

    Test client against latest snapshot that fixes guava classes issues.

--- build.sbt
+++ build.sbt
@@ -6,8 +6,8 @@ scalaVersion := "2.11.5"
 
 resolvers += "Local Maven Repository" at Path.userHome.asURL + "/.m2/repository"
 
-libraryDependencies += "junit" % "junit-dep" % "4.8" % "test"
+libraryDependencies += "junit" % "junit-dep" % "4.11" % "test"
 
-libraryDependencies += "org.scalatest" %% "scalatest" % "2.4.5" % "test"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.5" % "test"
 
-libraryDependencies += "org.mutabilitydetector" % "MutabilityDetector" % "0.10-SNAPSHOT" % "test"
+libraryDependencies += "org.mutabilitydetector" % "MutabilityDetector" % "0.9.4-SNAPSHOT" % "test"
--- src/test/scala/org/mutabilitydetector/scalaclient/ImmutabilityTests.scala
+++ src/test/scala/org/mutabilitydetector/scalaclient/ImmutabilityTests.scala
@@ -10,32 +10,8 @@ import org.mutabilitydetector.unittesting.MutabilityAssert._
 @RunWith(classOf[JUnitRunner])
 class ImmutabilityTests extends FunSuite with ShouldMatchers {
 
-  test("non final case class") {
-    assertImmutable(classOf[CaseClass])
-  }
-
   test("final case class") {
     assertImmutable(classOf[FinalCaseClass])
   }
 
-  test("final immutable class") {
-    assertImmutable(classOf[ImmutableClass])
-  }
-  
-  test("final mutable class with reassignable fields") {
-    assertImmutable(classOf[ReassignableFields])
-  }
-  
-  test("final immutable class with immutable list field") {
-    assertImmutable(classOf[ImmutableWithListField])
-  }
-  
-  test("scala.collection.immutable.List - abstract") {
-    assertImmutable(classOf[scala.collection.immutable.List[_]])
-  }
-
-  test("non final case class with immutable fields") {
-    MutabilityAssert.assertImmutable(classOf[SomeCaseClass])
-
-  }
 }
